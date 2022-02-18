a41723c025f09862557090d1f9f1eb1fd491c8ff
Yunus Elçi <56027479+yunuselci@users.noreply.github.com>
2022-02-18 13:09:11 +0300

    Silinen dosyaları geri yükledim
    Projeyi ilk oluşturduğumda auth. ekleyeceğimi düşünmediğimden bazı dosyaları silmiştim, geri yükledim

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WeatherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get("/weathers",[WeatherController::class, "index"]);
+    Route::post("/weathers", [WeatherController::class, "store"]);
+    Route::put("/weathers/{id}", [WeatherController::class, "update"]);
+    Route::delete("/weathers/{id}", [WeatherController::class, "destroy"]);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/weathers",[WeatherController::class, "index"]);
-Route::post("/weathers", [WeatherController::class, "store"]);
-Route::put("/weathers/{id}", [WeatherController::class, "update"]);
-Route::delete("/weathers/{id}", [WeatherController::class, "destroy"]);
 
